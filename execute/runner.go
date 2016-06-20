81b79a5b0f6b7ca858fcc0088812af9831ef1567
Xavi Soler <xsb@interrupciones.net>
2016-06-20 20:35:16 +0200

    Improve elapsed time format

--- execute/runner.go
+++ execute/runner.go
@@ -84,6 +84,26 @@ func findCycles(th map[string][]*types.Task) error {
 	return nil
 }
 
+func formatDuration(d time.Duration) (s string) {
+	timeMsg := ""
+
+	if d.Hours() > 1.0 {
+		timeMsg += fmt.Sprintf("%1.0fh", d.Hours())
+	}
+
+	if d.Minutes() > 1.0 {
+		timeMsg += fmt.Sprintf("%1.0fm", d.Minutes())
+	}
+
+	if d.Seconds() > 1.0 {
+		timeMsg += fmt.Sprintf("%1.0fs", d.Seconds())
+	} else {
+		timeMsg += fmt.Sprintf("%1.3fs", d.Seconds())
+	}
+
+	return timeMsg
+}
+
 // NewRunner creates a new runner that contains a list of all execution paths.
 func NewRunner(tm types.TaskMap) (*runner, error) {
 	th, err := buildHierarchy(tm)
@@ -146,7 +166,7 @@ func (r *runner) waitFor(taskName string) {
 			case "end":
 				if statusCode, ok := event.Extras["statusCode"].(int); ok {
 					if elapsed, ok := event.Extras["elapsed"].(time.Duration); ok {
-						fmt.Printf(" - %s took %s and finished with status code %d\n", event.Task, elapsed.String(), statusCode)
+						fmt.Printf(" - %s took %s and finished with status code %d\n", event.Task, formatDuration(elapsed), statusCode)
 
 						if statusCode != 0 || event.Task == taskName {
 							os.Exit(statusCode)
